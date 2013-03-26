1cf051d290b68befd6208de1d3f625f27d52b833
Arron Green <arrong@gmail.com>
2013-03-26 16:51:19 -0400

    added hadoop module

---
+++ modules/hadoop/manifests/init.pp
@@ -0,0 +1,56 @@
+class hadoop {
+  $hadoop_home = "/etc/hadoop"
+
+  package { ["hadoop-hdfs"]:
+    ensure => "installed"
+  }
+
+  file { "${hadoop_path}/conf/slaves":
+    source => "puppet:///modules/hadoop/slaves",
+    mode => 644,
+    owner => root,
+    group => root,
+    require => Package["hadoop-hdfs"]
+  }
+
+  file { "${hadoop_path}/conf/masters":
+    source => "puppet:///modules/hadoop/masters",
+    mode => 644,
+    owner => root,
+    group => root,
+    require => Package["hadoop-hdfs"]
+  }
+
+  file { "${hadoop_path}/conf/core-site.xml":
+    source => "puppet:///modules/hadoop/core-site.xml",
+    mode => 644,
+    owner => root,
+    group => root,
+    require => Package["hadoop-hdfs"]
+  }
+
+  file { "${hadoop_path}/conf/mapred-site.xml":
+    source => "puppet:///modules/hadoop/mapred-site.xml",
+    mode => 644,
+    owner => root,
+    group => root,
+    require => Package["hadoop-hdfs"]
+  }
+
+  file { "hdfs-site":
+    path => "${hadoop_path}/conf/hdfs-site.xml",
+    mode => 644,
+    owner => root,
+    group => root,
+    content => template("hadoop/hdfs-site.xml.erb"),
+    require => Package["hadoop-hdfs"]
+  }
+
+  file { "${hadoop_path}/conf/hadoop-env.sh":
+    source => "puppet:///modules/hadoop/hadoop-env.sh",
+    mode => 644,
+    owner => root,
+    group => root,
+    require => Package["hadoop-hdfs"]
+  }
+}
