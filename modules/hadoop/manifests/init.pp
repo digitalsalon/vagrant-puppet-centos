5d548eeaf0c94351e365fb9037d5b25305a1155a
Arron Green <arrong@gmail.com>
2013-03-26 18:15:34 -0400

    verbose with file paths

--- modules/hadoop/manifests/init.pp
+++ modules/hadoop/manifests/init.pp
@@ -5,52 +5,57 @@ class hadoop {
     ensure => "installed"
   }
 
-  file { "${hadoop_path}/conf/slaves":
-    source => "puppet:///modules/hadoop/slaves",
-    mode => 644,
-    owner => root,
-    group => root,
+  file { "slaves":
+    path    => "${hadoop_path}/conf/slaves",
+    source  => "puppet:///modules/hadoop/slaves",
+    mode    => 644,
+    owner   => root,
+    group   => root,
     require => Package["hadoop-hdfs"]
   }
 
-  file { "${hadoop_path}/conf/masters":
-    source => "puppet:///modules/hadoop/masters",
-    mode => 644,
-    owner => root,
-    group => root,
+  file { "masters":
+    path    => "${hadoop_path}/conf/masters",
+    source  => "puppet:///modules/hadoop/masters",
+    mode    => 644,
+    owner   => root,
+    group   => root,
     require => Package["hadoop-hdfs"]
   }
 
-  file { "${hadoop_path}/conf/core-site.xml":
-    source => "puppet:///modules/hadoop/core-site.xml",
-    mode => 644,
-    owner => root,
-    group => root,
+  file { "core-site.xml":
+    path    => "${hadoop_path}/conf/core-site.xml",
+    source  => "puppet:///modules/hadoop/core-site.xml",
+    mode    => 644,
+    owner   => root,
+    group   => root,
     require => Package["hadoop-hdfs"]
   }
 
-  file { "${hadoop_path}/conf/mapred-site.xml":
-    source => "puppet:///modules/hadoop/mapred-site.xml",
-    mode => 644,
-    owner => root,
-    group => root,
+  file { "mapred-site.xml":
+    path    => "${hadoop_path}/conf/mapred-site.xml",
+    source  => "puppet:///modules/hadoop/mapred-site.xml",
+    mode    => 644,
+    owner   => root,
+    group   => root,
     require => Package["hadoop-hdfs"]
   }
 
-  file { "hdfs-site":
-    path => "${hadoop_path}/conf/hdfs-site.xml",
-    mode => 644,
-    owner => root,
-    group => root,
+  file { "hdfs-site.xml":
+    path    => "${hadoop_path}/conf/hdfs-site.xml",
+    mode    => 644,
+    owner   => root,
+    group   => root,
     content => template("hadoop/hdfs-site.xml.erb"),
-    require => Package["hadoop-hdfs"]
+    require => Package["hadoop-hdfs"],
   }
 
-  file { "${hadoop_path}/conf/hadoop-env.sh":
-    source => "puppet:///modules/hadoop/hadoop-env.sh",
-    mode => 644,
-    owner => root,
-    group => root,
+  file { "hadoop-env.sh":
+    path    => "${hadoop_path}/conf/hadoop-env.sh",
+    source  => "puppet:///modules/hadoop/hadoop-env.sh",
+    mode    => 644,
+    owner   => root,
+    group   => root,
     require => Package["hadoop-hdfs"]
   }
 }
