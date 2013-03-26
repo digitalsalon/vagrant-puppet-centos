b483967e3051a4308f7e3edd017b39a0b1323076
Arron Green <arrong@gmail.com>
2013-03-26 14:12:12 -0400

    install of cloudera is working

--- modules/cloudera/manifests/init.pp
+++ modules/cloudera/manifests/init.pp
@@ -1,10 +1,25 @@
 class cloudera{
   $cloudera_version = "cloudera-cdh-4-0.x86_64"
-  $cloudera_url = "http://archive.cloudera.com/cdh4/one-click-install/redhat/6/x86_64/${cloudera_version}.rpm"
+  $cd4 = "http://archive.cloudera.com/cdh4"
+  $cloudera_url = "${cd4}/one-click-install/redhat/6/x86_64/${cloudera_version}.rpm"
+  $cloudera_rpm = "${cd4}/redhat/6/x86_64/cdh/RPM-GPG-KEY-cloudera"
 
-  exec { "download cloudera":
+  exec { "download_cloudera":
     command => "wget -O /tmp/${cloudera_version}.rpm ${cloudera_url}",
     path    => $path,
     unless  => "ls /tmp | grep ${cloudera_version}.rpm"
+  }
+
+  #todo: figure out how to use package to install a local RPM
+  exec { "install_cloudera":
+    command => "/usr/bin/yum -d 0 -e 0 -y --nogpgcheck localinstall /tmp/${cloudera_version}.rpm",
+    path    => $path,
+    require => Exec["download_cloudera"],
+  }
+
+  exec { "cloudera_rpm_gpg_key":
+    command => "/bin/rpm --import ${cloudera_rpm}",
+    path    => $path,
+    require => Exec["install_cloudera"],
   }
 }
