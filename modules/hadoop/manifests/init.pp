bef04a8983eff4f4a9c6672191bc7785ca5c9d33
Arron Green <arrong@gmail.com>
2013-03-27 00:26:56 -0400

    added path, specified replace and fixed path variable name

--- modules/hadoop/manifests/init.pp
+++ modules/hadoop/manifests/init.pp
@@ -6,56 +6,62 @@ class hadoop {
   }
 
   file { "slaves":
-    path    => "${hadoop_path}/conf/slaves",
+    path    => "${hadoop_home}/conf/slaves",
     source  => "puppet:///modules/hadoop/slaves",
     mode    => 644,
     owner   => root,
     group   => root,
+    replace => true,
     require => Package["hadoop-hdfs"]
   }
 
   file { "masters":
-    path    => "${hadoop_path}/conf/masters",
+    path    => "${hadoop_home}/conf/masters",
     source  => "puppet:///modules/hadoop/masters",
     mode    => 644,
     owner   => root,
     group   => root,
+    replace => true,
     require => Package["hadoop-hdfs"]
   }
 
   file { "core-site.xml":
-    path    => "${hadoop_path}/conf/core-site.xml",
+    path    => "${hadoop_home}/conf/core-site.xml",
     source  => "puppet:///modules/hadoop/core-site.xml",
     mode    => 644,
     owner   => root,
     group   => root,
+    replace => true,
     require => Package["hadoop-hdfs"]
   }
 
   file { "mapred-site.xml":
-    path    => "${hadoop_path}/conf/mapred-site.xml",
+    path    => "${hadoop_home}/conf/mapred-site.xml",
     source  => "puppet:///modules/hadoop/mapred-site.xml",
     mode    => 644,
     owner   => root,
     group   => root,
+    replace => true,
     require => Package["hadoop-hdfs"]
   }
 
   file { "hdfs-site.xml":
-    path    => "${hadoop_path}/conf/hdfs-site.xml",
+    path    => "${hadoop_home}/conf/hdfs-site.xml",
     mode    => 644,
     owner   => root,
     group   => root,
     content => template("hadoop/hdfs-site.xml.erb"),
+    replace => true,
     require => Package["hadoop-hdfs"],
   }
 
   file { "hadoop-env.sh":
-    path    => "${hadoop_path}/conf/hadoop-env.sh",
+    path    => "${hadoop_home}/conf/hadoop-env.sh",
     source  => "puppet:///modules/hadoop/hadoop-env.sh",
     mode    => 644,
     owner   => root,
     group   => root,
+    replace => true,
     require => Package["hadoop-hdfs"]
   }
 }
