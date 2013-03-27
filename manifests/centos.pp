d235e00c9fcec921c85bada4e1d6c044c38f2939
Arron Green <arrong@gmail.com>
2013-03-27 12:24:07 -0400

    added java home as global variable and added hbase to dependency chain

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,3 +1,5 @@
+$java_home = "/usr/lib/jvm/jre-openjdk"
+
 class{'cloudera': }
-class{'java': } -> class{'hadoop': }
+class{'java': } -> class{'hbase': java_home =>  $java_home} -> class{'hadoop': java_home =>$java_home}
 class{'sshconfig': }
