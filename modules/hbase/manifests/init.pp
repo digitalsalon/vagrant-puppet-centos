c9dc9c5672479a78ee14074a32b5c90ec153ce18
Arron Green <arrong@gmail.com>
2013-03-27 12:42:34 -0400

    changes to hbase sh file

--- modules/hbase/manifests/init.pp
+++ modules/hbase/manifests/init.pp
@@ -4,4 +4,14 @@ class hbase {
   package { ["hbase"]:
     ensure => "installed"
   }
+
+  file { "hadoop-env.sh":
+    path    => "${hbase_home}/conf/hbase-env.sh",
+    content => template("hbase/hbase-env.sh.erb"),
+    mode    => 644,
+    owner   => root,
+    group   => root,
+    replace => true,
+    require => Package["hbase"]
+  }
 }
