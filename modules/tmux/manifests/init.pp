2f7d0b9c22686c23172fbf98c42ac543b8c28dcc
Arron Green <arrong@gmail.com>
2013-03-27 13:03:18 -0400

    forgot to include Exec with tmux

--- modules/tmux/manifests/init.pp
+++ modules/tmux/manifests/init.pp
@@ -1,12 +1,12 @@
 class tmux {
   $tmux_rpm = "http://download.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm"
-  
-  package { "tmux":
-    require => "tmux_rpm",
-  }
-  
+
   exec { "tmux_rpm" :
     command => "/bin/rpm --import ${tmux_rpm}",
     path    => $path,
+  }
+
+  package { ["tmux"]:
+    require => Exec["tmux_rpm"],
   }
 }
