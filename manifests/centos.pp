08609bd35f971fca27540c0fdcf102c31a046529
Arron Green <arrong@gmail.com>
2013-03-27 12:55:15 -0400

    added tmux

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -3,3 +3,4 @@ $java_home = "/usr/lib/jvm/jre-openjdk"
 class{'cloudera': }
 class{'java': } -> class{'hbase': java_home =>  $java_home} -> class{'hadoop': java_home =>$java_home}
 class{'sshconfig': }
+class{'tmux': }
