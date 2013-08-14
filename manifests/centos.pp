5bfbb25eb862ebfb39d78017e410732967139da1
Arron Green <arrong@gmail.com>
2013-08-13 20:19:22 -0400

    cp

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,6 +1,7 @@
 $java_home = "/usr/lib/jvm/jre-openjdk"
 
 class{'cloudera': }
-class{'java': } -> class{'hbase': java_home =>  $java_home} -> class{'hadoop': java_home =>$java_home}
+# class{'java': } -> class{'hbase': java_home =>  $java_home} -> class{'hadoop': java_home =>$java_home}
+class{'java': } -> class{'hadoop': java_home =>$java_home}
 class{'sshconfig': }
 class{'tmux': }
